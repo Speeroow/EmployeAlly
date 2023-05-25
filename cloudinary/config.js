1ae7530362fdb3fffb51ae4834e2a6da479680c4
Speeroow <110326448+Speeroow@users.noreply.github.com>
2023-05-25 19:51:32 +0530

    Update config.js

--- cloudinary/config.js
+++ cloudinary/config.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
 	cloudinary,
 	params: {
-		folder: 'HireHub',
+		folder: 'EmployeAlly',
 		allowedFormats: [ 'jpeg', 'png', 'jpg' ]
 	}
 });
